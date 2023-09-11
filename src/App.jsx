394ee172438aa5834ef6d3166ebf81ec675f4286
Roshan <roshanpokhrel013@gmail.com>
2023-09-11 15:35:27 +0545

    Refactored Some Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import React, { useState } from 'react'
 import Home from './pages/Home'
-import Navbar from './components/navbar'
-import Announcement from './components/Announcement'
-import './App.css'
-import Slider from './components/Slider'
-import Catagories from './components/Catagories'
-import Products from './components/Products'
-import Newsletter from './components/Newsletter'
-import Footer from './components/Footer'
+
 
 export default function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-    <Announcement/>
-    <Navbar/>
-    <Slider/>
-    <Catagories/>
-    <Products/>
-    <Newsletter/>
-    <Footer/>
+    <Home/>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import React, { useState } from 'react'
+import Navbar from '../components/navbar'
+import Announcement from '../components/Announcement'
+import '../App.css'
+import Slider from '../components/Slider'
+import Catagories from '../components/Catagories'
+import Products from '../components/Products'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
 
 export default function Home() {
   //const [count, setCount] = useState(0)
 
   return (
-     <p>This is Home!</p>
+    <>
+    <Announcement/>
+    <Navbar/>
+    <Slider/>
+    <Catagories/>
+    <Products/>
+    <Newsletter/>
+    <Footer/>
+    </>
   )
 }
 
